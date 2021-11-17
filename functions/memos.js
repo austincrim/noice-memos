fc438c6ac00cad4bcf0de1178d50ddbf9972cfab
Austin Crim <aust.crim@gmail.com>
2021-11-17 09:22:42 -0600

    idk

--- functions/memos.js
+++ functions/memos.js
@@ -1,30 +1,30 @@
 // const CORS_HEADERS = {
 //   'access-control-allow-origin': 'https://noice-memos.pages.dev',
-//   // 'access-control-allow-origin': 'http://localhost:5000',
+//   'access-control-allow-origin': 'http://localhost:3000',
 //   'access-control-allow-headers': 'content-type',
-//   'access-control-allow-methods': '*',
+//   'access-control-allow-methods': '*'
 // }
 
-// addEventListener('fetch', event => {
-//   if (event.request.method === 'OPTIONS') {
-//     event.respondWith(
-//       new Response(null, {
-//         headers: CORS_HEADERS,
-//       }),
-//     )
-//   } else if (event.request.method === 'GET') {
-//     event.respondWith(handleGet(event.request))
-//   } else if (event.request.method === 'PUT') {
-//     event.respondWith(handlePut(event.request))
+// export const onRequestOptions = ({ request }) => {
+//   if (request.method === 'OPTIONS') {
+//     return new Response(null, {
+//       headers: CORS_HEADERS
+//     })
 //   }
-// })
+// }
 
 export const onRequestGet = async ({ request, env }) => {
   const ip = request.headers.get('cf-connecting-ip')
-  const ipMemos = await env.memos.get(ip)
-  return new Response(JSON.stringify(ipMemos), {
-    headers: { 'content-type': 'application/json' }
-  })
+  console.log(env)
+  try {
+    const ipMemos = await env.memos.get(ip)
+    return new Response(JSON.stringify(ipMemos), {
+      headers: { 'content-type': 'application/json' }
+    })
+  } catch (e) {
+    // console.error(e)
+    return new Response(JSON.stringify(e))
+  }
 }
 
 export async function onRequestPut({ request, env }) {
