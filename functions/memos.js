2c1efc515cd074c8e5389bda926e9e5ad82799cc
Austin Crim <aust.crim@gmail.com>
2021-11-17 09:45:11 -0600

    clean up

--- functions/memos.js
+++ functions/memos.js
@@ -1,18 +1,3 @@
-// const CORS_HEADERS = {
-//   'access-control-allow-origin': 'https://noice-memos.pages.dev',
-//   'access-control-allow-origin': 'http://localhost:3000',
-//   'access-control-allow-headers': 'content-type',
-//   'access-control-allow-methods': '*'
-// }
-
-// export const onRequestOptions = ({ request }) => {
-//   if (request.method === 'OPTIONS') {
-//     return new Response(null, {
-//       headers: CORS_HEADERS
-//     })
-//   }
-// }
-
 export const onRequestGet = async ({ request, env }) => {
   const ip = request.headers.get('cf-connecting-ip')
   try {
