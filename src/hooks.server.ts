db35fb55ed11dc3f5bd70e679385d9e8f11e952c
Austin Crim <aust.crim@gmail.com>
2022-12-24 16:48:50 -0600

    update dep

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,11 @@
-import Auth from '@auth/sveltekit'
+import { SvelteKitAuth } from '@auth/sveltekit'
 import GitHub from '@auth/core/providers/github'
 import D1Adapter from '$lib/adapter'
 import { GH_CLIENT_ID, GH_CLIENT_SECRET } from '$env/static/private'
-import type { RequestEvent } from '@sveltejs/kit'
 import type { Handle } from '@sveltejs/kit'
 
 export const handle: Handle = ({ event, resolve }) => {
-  let authHandle = Auth({
+  let authHandle = SvelteKitAuth({
     providers: [
       GitHub({ clientId: GH_CLIENT_ID, clientSecret: GH_CLIENT_SECRET })
     ],
