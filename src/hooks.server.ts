f2afaaed0a5d7b32a3a853e942888151f071fce1
Austin Crim <aust.crim@gmail.com>
2022-12-29 10:11:09 -0600

    yeeeesh

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,19 +4,29 @@ import D1Adapter from '$lib/adapter'
 import { GH_CLIENT_ID, GH_CLIENT_SECRET } from '$env/static/private'
 import type { Handle } from '@sveltejs/kit'
 
-export const handle: Handle = async ({ event, resolve }) => {
+export const handle: Handle = ({ event, resolve }) => {
   let authHandle = SvelteKitAuth({
     providers: [
-      GitHub({ clientId: GH_CLIENT_ID, clientSecret: GH_CLIENT_SECRET })
+      GitHub({
+        clientId: GH_CLIENT_ID,
+        clientSecret: GH_CLIENT_SECRET,
+        userinfo: {
+          url: 'https://api.github.com/user',
+          async request({ provider, tokens }) {
+            const profile = await fetch('https://api.github.com/user', {
+              headers: {
+                Authorization: `Bearer ${tokens.access_token}`,
+                'User-Agent': 'my-fun-worker'
+              }
+            }).then(async (res) => await res.json())
+            return profile
+          }
+        }
+      })
     ],
     adapter: D1Adapter(event.platform.env.DB),
     trustHost: true
   })
-  let response = authHandle({ event, resolve })
-  if (typeof response === 'object' && typeof response.then === 'function') {
-    console.log(JSON.stringify(await response, null, 2))
-  } else {
-    console.log(await response.text())
-  }
-  return response
+
+  return authHandle({ event, resolve })
 }
