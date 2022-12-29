a37a001f8212a494f88df79f335d8ba0f417a4cc
Austin Crim <aust.crim@gmail.com>
2022-12-28 21:15:01 -0600

    more logs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,7 +4,7 @@ import D1Adapter from '$lib/adapter'
 import { GH_CLIENT_ID, GH_CLIENT_SECRET } from '$env/static/private'
 import type { Handle } from '@sveltejs/kit'
 
-export const handle: Handle = ({ event, resolve }) => {
+export const handle: Handle = async ({ event, resolve }) => {
   let authHandle = SvelteKitAuth({
     providers: [
       GitHub({ clientId: GH_CLIENT_ID, clientSecret: GH_CLIENT_SECRET })
@@ -13,6 +13,10 @@ export const handle: Handle = ({ event, resolve }) => {
     trustHost: true
   })
   let response = authHandle({ event, resolve })
-  console.log(JSON.stringify(response, null, 2))
+  if (typeof response === 'object' && typeof response.then === 'function') {
+    console.log(JSON.stringify(await response, null, 2))
+  } else {
+    console.log(await response.text())
+  }
   return response
 }
