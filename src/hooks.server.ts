39962d2072ae69d39c0717b8f42324aa6c9cbbed
Austin Crim <aust.crim@gmail.com>
2023-01-17 17:05:48 -0600

    shrug

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,33 +1,37 @@
 import type { Handle } from '@sveltejs/kit'
-import { dev } from '$app/environment'
 import { SvelteKitAuth } from '@auth/sveltekit'
 import GitHub from '@auth/core/providers/github'
 import D1Adapter from '$lib/adapter'
-import { GH_CLIENT_ID, GH_CLIENT_SECRET } from '$env/static/private'
+import {
+  GH_CLIENT_ID,
+  GH_CLIENT_SECRET,
+  AUTH_SECRET
+} from '$env/static/private'
 
 export const handle: Handle = ({ event, resolve }) => {
   let authHandle = SvelteKitAuth({
     providers: [
       GitHub({
         clientId: GH_CLIENT_ID,
-        clientSecret: GH_CLIENT_SECRET,
-        userinfo: {
-          url: 'https://api.github.com/user',
-          async request({ tokens }) {
-            const profile = await fetch('https://api.github.com/user', {
-              headers: {
-                Authorization: `Bearer ${tokens.access_token}`,
-                'User-Agent': 'my-fun-worker'
-              }
-            }).then(async (res) => await res.json())
-            return profile
-          }
-        }
+        clientSecret: GH_CLIENT_SECRET
+        // userinfo: {
+        //   url: 'https://api.github.com/user',
+        //   async request({ tokens }) {
+        //     const profile = await fetch('https://api.github.com/user', {
+        //       headers: {
+        //         Authorization: `Bearer ${tokens.access_token}`,
+        //         'User-Agent': 'my-fun-worker'
+        //       }
+        //     }).then(async (res) => await res.json())
+        //     return profile
+        //   }
+        // }
       })
     ],
     adapter: D1Adapter(event.platform.env.DB),
-    trustHost: true
+    trustHost: true,
+    secret: AUTH_SECRET
   })
-  // console.log(event.platform.env)
+
   return authHandle({ event, resolve })
 }
